edf673f76bf8565830b23c8c09a430326642e4e0
eitanacton <eitanacton@gmail.com>
2015-08-03 16:25:55 +0300

    f

--- C15 Ex01 MorZilberman 203571385 EitanActon 039373162/FacebookIntegrationExcercise/UserInfo.cs
+++ C15 Ex01 MorZilberman 203571385 EitanActon 039373162/FacebookIntegrationExcercise/UserInfo.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
 namespace FacebookIntegrationExcercise
 {
     class UserInfo
     {
-
+        public string AccessToken { private set; get; }
+        public Point Location { private set; get; }
+        public Size Size { private set; get; }
+        public bool AutoLogIn { private set; get; }
     }
 }
