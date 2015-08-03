9fbc1983f6527903f83af8e58216c2df6d3327b6
Paximillian <morzil@walla.com>
2015-08-03 15:04:47 +0300

    Threaded login

--- C15 Ex01 MorZilberman 203571385 EitanActon 039373162/FacebookIntegrationExcercise/Form1.cs
+++ C15 Ex01 MorZilberman 203571385 EitanActon 039373162/FacebookIntegrationExcercise/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using FacebookWrapper;
 using FacebookWrapper.ObjectModel;
+using System.Threading;
 
 namespace FacebookIntegrationExcercise
 {
@@ -21,7 +22,24 @@ namespace FacebookIntegrationExcercise
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
+            Thread loginThread = new Thread(new ThreadStart(requestLoginDetails));
+            loginThread.SetApartmentState(ApartmentState.STA);
+            loginThread.Start();
+        }
+
+        private void requestLoginDetails()
+        {
             LoginResult loginResult = FacebookService.Login("1519758448246535", new string[] { "public_profile", "email", "user_friends" });
+            
+            if (loginResult.ErrorMessage != string.Empty)
+            {
+                MessageBox.Show(string.Format("Error logging into Facebook!{0}The error returned was:{0}{1}", Environment.NewLine, loginResult.ErrorMessage), "Error logging in", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
