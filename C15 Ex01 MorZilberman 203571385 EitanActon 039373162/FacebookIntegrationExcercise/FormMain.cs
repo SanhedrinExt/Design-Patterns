cf978d9daea069f96876d2d7b45261975c0f3930
eitanacton <eitanacton@gmail.com>
2015-08-03 16:08:40 +0300

    a

--- C15 Ex01 MorZilberman 203571385 EitanActon 039373162/FacebookIntegrationExcercise/FormMain.cs
+++ C15 Ex01 MorZilberman 203571385 EitanActon 039373162/FacebookIntegrationExcercise/FormMain.cs
@@ -20,17 +20,31 @@ namespace FacebookIntegrationExcercise
             InitializeComponent();
         }
 
-        private void buttonLogin_Click(object sender, EventArgs e)
+        User m_LoggedInUser;
+
+        private void loginAndInit()
         {
             LoginResult loginResult = FacebookService.Login("1519758448246535", new string[] { "public_profile", "email", "user_friends" });
 
-            if (loginResult.ErrorMessage != string.Empty)
+            if (string.IsNullOrEmpty(loginResult.ErrorMessage))
             {
                 MessageBox.Show(string.Format("Error logging into Facebook!{0}The error returned was:{0}{1}", Environment.NewLine, loginResult.ErrorMessage), "Error logging in", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
+                m_LoggedInUser = loginResult.LoggedInUser;
+                //fetchUserInfo();  TO DO
             }
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
         }
 
         private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
